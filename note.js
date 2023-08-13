f1a63360664f6b1666dfa7f7b3c96bd6c4b7e4b2
fakhrul3g <fakhrul3g@gmail.com>
2023-08-14 04:44:21 +0600

    initial commit

---
+++ note.js
@@ -0,0 +1,8 @@
+// checking node present or not in cmd
+//     node -v
+
+// to create package.json write the code in terminal
+//     npm init --y
+
+// to install nodemon development dependencies write in terminal
+//     npm install nodemon -D
