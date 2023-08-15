39b3815a36eecece00a3ce2329ebaa4b457cf0b5
fakhrul3g <fakhrul3g@gmail.com>
2023-08-15 23:04:18 +0600

    updated

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
- console.log("hellow world")
- console.log("hellow world4")
+// let fruits=["apple", "Banana", "Guava"]
+// fruits[2]="Grape";
+// console.log(fruits[2])
+
+let person={
+    name:"Mahmud",
+    age:35,
+    isStudent:true,
+    Address:{
+        road:03,
+        house:'3A',
+        city:"Chattogram",
+        country:"Bangladesh"
+    }
+    educaton:{
+        BSc:2012,
+        Diploma:2006,
+institute:["UITS","CPI","CSM"]
+    }
+}
+
+let personInfo=`My name is ${person.name}`
+
+console.log(personInfo)
